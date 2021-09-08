f07973f316ed06b390a90403ba41b2bb6908fe77
ClaudiaCZ <claudiavcasco@gmail.comgit config --global user.email git config --global user.email claudiavcasco@gmail.comgit config --global user.email claudiavcasco@gmail.com>
2021-09-08 17:10:00 -0300

    terminado Login y Register

--- app.js
+++ app.js
@@ -2,9 +2,13 @@ const express = require('express');
 const app = express();
 const porth = 3030;
 const path = require('path');
+const user = require('./routes/user');
+const index = require('./routes/index')
+
 
 app.use(express.static('public'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));
+/*app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'views', 'home.html')));*/
 
-
+app.use('/user', user);
+app.use('/', index);
 app.listen(porth, () => console.log('Servidor ejecutando en el puerto ', porth));
